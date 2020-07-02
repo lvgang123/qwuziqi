defe4b1d66f934b976466f7b2af1145da804d025
admin <1341368321@qq.com>
2020-07-02 17:28:20 +0800

    2020-7-2 ver0.1

--- main.cpp
+++ main.cpp
@@ -6,13 +6,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-
     AppInit::Instance()->start();
-
-    qcout<<&APP::gameMapVec;
-
-    if(APP::gameMapVec.size()>0 && APP::gameMapVec[0].size()>0)
-        qcout<<APP::gameMapVec.at(0).at(0);
 
     return a.exec();
 }
